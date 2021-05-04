24600f3aa275de55ed16ac63f4fcaf5a78646893
Dejan Dolenc <dejdolenc@gmail.com>
2021-05-04 18:05:54 +0200

    Removed built files

--- dist/PbTranscoder.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-export declare function decode(buf: Buffer): {
-    [k: string]: any;
-};
-export declare function encode(obj: Record<string, any>): Buffer;
--- dist/idCreator.d.ts
+++
@@ -1,7 +0,0 @@
-export default class IdCreator {
-    min: number;
-    max: number;
-    curId: number;
-    constructor(min?: number, max?: number);
-    next(): number;
-}
